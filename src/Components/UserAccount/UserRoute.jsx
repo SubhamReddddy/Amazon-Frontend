cd8e7066ab1d52d68442e7590642567793c99a1c
SubhamReddddy <subhamreddy121@gmail.com>
2024-05-30 09:39:02 +0530

    userRouter issue solved!

--- src/Components/UserAccount/UserRoute.jsx
+++ src/Components/UserAccount/UserRoute.jsx
@@ -3,9 +3,18 @@ import Header from "../Header/Header";
 import { Navigate, Outlet, ScrollRestoration } from "react-router-dom";
 import Bottom from "../Bottom/Bottom";
 import { useSelector } from "react-redux";
+import Loading from "../Loader/Loading";
 
 const UserRoute = () => {
   const { user } = useSelector((State) => State.userSlice);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  });
+  if (loading) return <Loading />;
   return user._id ? (
     <div>
       <Header />
